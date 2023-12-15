45a635386cdee14f4e33fcdc168a4ca3b786fc45
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-12-14 23:51:26 -0300

    Feat: Implementação da interface e classe base para os serviços da aplicação.

---
+++ Domain/Services/Generic/BaseInterface.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+using Domain.Repositories;
+
+namespace Domain.Services.Generic
+{
+    public class BaseInterface<T> : IBaseInterface<T> where T : AggregateRoot, new()
+    {
+        private readonly IStreamRepository<T> _repository;
+
+        public BaseInterface(IStreamRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public virtual async Task<T> GetAsync(string id)
+        {
+            return await _repository.GetAsync(id);
+        }
+
+        public virtual async Task SaveAsync(T entity)
+        {
+            await _repository.SaveAsync(entity);
+        }
+    }
+}
---
+++ Domain/Services/Generic/IBaseInterface.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Domain.Services.Generic
+{
+    public interface IBaseInterface<T> where T : AggregateRoot
+    {
+        Task<T> GetAsync(string id);
+        Task SaveAsync(T entity);
+    }
+}
