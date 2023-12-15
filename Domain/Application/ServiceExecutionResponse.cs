41d0aa609062b88204403d5e7bc00dc2efa5755f
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-12-14 23:50:49 -0300

    Feat: Classe para encapsular o retorno dos serviços da aplicação.

---
+++ Domain/Application/ServiceExecutionResponse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Domain.Application
+{
+    public class ServiceExecutionResponse
+    {
+        public ServiceExecutionResponse(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+        }
+
+        public ServiceExecutionResponse(object? data)
+        {
+            IsSuccess = data != null;
+            Data = data;
+        }
+
+        public ServiceExecutionResponse(Exception error)
+        {
+            IsSuccess = false;
+            Error = error;
+            Message = error.Message;
+        }
+
+        public ServiceExecutionResponse(bool isSuccess, string? message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+
+        public ServiceExecutionResponse(bool isSuccess, object? data)
+        {
+            IsSuccess = isSuccess;
+            Data = data;
+        }
+
+        public ServiceExecutionResponse(string? message, Exception error)
+        {
+            IsSuccess = false;
+            Message = message;
+            Error = error;
+        }
+
+        public ServiceExecutionResponse(bool isSuccess, string? message, object? data)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+            Data = data;
+        }
+
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+        public object? Data { get; set; }
+        public Exception? Error { get; set; }
+    }
+}
