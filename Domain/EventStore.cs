0dbc58fc219ef94360d66bb2c8f7a054c27ffe4b
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-12-17 16:31:26 -0300

    Fix: Correção no encapsulamento das classes/interfaces.

--- Domain/EventStore.cs
+++ Domain/EventStore.cs
@@ -3,7 +3,7 @@ using Microsoft.Azure.Cosmos;
 
 namespace System
 {
-    public class EventStore<T> : EventStore, IEventStore<T>
+    internal class EventStore<T> : EventStore, IEventStore<T>
     {
         public EventStore(CosmosClient client, string database, string container, EventStoreOptions options = null) : base(client, database, container, options)
         {
