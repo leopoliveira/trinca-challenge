6e14ea3ddc8ef94dc6b552106153b433847fd874
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-12-17 09:53:26 -0300

    Refactor: Removendo espaço em branco.

--- CrossCutting/ServiceCollectionExtensions.cs
+++ CrossCutting/ServiceCollectionExtensions.cs
@@ -9,7 +9,7 @@ namespace CrossCutting
     public static class ServiceCollectionExtensions
     {
         private const string DATABASE = "Churras";
-        public static IServiceCollection AddEventStore(this IServiceCollection services) 
+        public static IServiceCollection AddEventStore(this IServiceCollection services)
         {
             var client = new CosmosClient(Environment.GetEnvironmentVariable(nameof(EventStore)));
 
@@ -22,7 +22,7 @@ namespace CrossCutting
 
         private async static Task CreateIfNotExists(this CosmosClient client, string database, string collection)
         {
-            var databaseResponse = await client.CreateDatabaseIfNotExistsAsync(database);
+                var databaseResponse = await client.CreateDatabaseIfNotExistsAsync(database);
             await databaseResponse.Database.CreateContainerIfNotExistsAsync(new ContainerProperties(collection, "/StreamId"));
         }
     }
